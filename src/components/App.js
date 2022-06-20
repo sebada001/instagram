d21f8a3cb0233ff17dcb3fe99f81a393bc17bbbb
Sebastian Schumacher <sebastianschumacher10@gmail.com>
2022-06-20 16:20:47 -0400

    create and organize files, create a basic FeedPost component

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import FeedPost from "./FeedPost";
+
+function App() {
+  return (
+    <div className="App">
+      <FeedPost />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FeedPost.js
@@ -0,0 +1,36 @@
+import bottom from "../svg/bottom.svg";
+import top from "../svg/top.svg";
+import right from "../svg/right.svg";
+import left from "../svg/left.svg";
+import circle from "../svg/circle.svg";
+import pic from "../img/sc.jpg";
+import opm from "../img/opm.png";
+import "../styles/App.css";
+
+function FeedPost() {
+  return (
+    <div className="Post">
+      <div id="square">
+        <img src={left} className="left" alt="left" />
+        <img src={top} className="top" alt="top" />
+        <img src={bottom} className="bottom" alt="bottom" />
+        <img src={bottom} className="inner-bottom" alt="inner-bottom" />
+        <img src={right} className="right" alt="right" />
+        <div className="title-post">
+          <img src={circle} className="circle" alt="circle" />
+          <div className="profile-container">
+            <img src={pic} className="sonic" alt="sonic" />
+          </div>
+          <h3>User - Title</h3>
+        </div>
+
+        <div className="image-post">
+          <img src={opm} className="opm" alt="opm" />
+        </div>
+      </div>
+
+      <p>Save to reload.</p>
+    </div>
+  );
+}
+export default FeedPost;
