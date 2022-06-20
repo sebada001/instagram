aa912f550a1664b2d95665f82572a1767c5b66e4
Sebastian Schumacher <sebastianschumacher10@gmail.com>
2022-06-20 16:24:48 -0400

    install firebase and init app

---
+++ src/firebase/app-init.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvNVHUdO4IDBsKEe1c_F4BV7oZjJNFKn8",
+
+  authDomain: "instagram-clone-2a893.firebaseapp.com",
+
+  projectId: "instagram-clone-2a893",
+
+  storageBucket: "instagram-clone-2a893.appspot.com",
+
+  messagingSenderId: "777055006435",
+
+  appId: "1:777055006435:web:e240059084f4d78beb1fd6",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
