10c10fc970040eebc6005cacb1190460c13d842c
Sebastian Schumacher <sebastianschumacher10@gmail.com>
2022-06-30 17:14:42 -0400

    dynamic posts from GeneralFeed - FeedPost using an array of objects

--- src/components/FeedPost.js
+++ src/components/FeedPost.js
@@ -3,15 +3,10 @@ import top from "../svg/top.svg";
 import right from "../svg/right.svg";
 import left from "../svg/left.svg";
 import circle from "../svg/circle.svg";
-import pic from "../img/sc.jpg";
-import opm from "../img/opm.png";
 
-const postImage =
-  "https://firebasestorage.googleapis.com/v0/b/instagram-clone-2a893.appspot.com/o/testingimage.jpg?alt=media&token=0d66dd18-63df-4ff8-bfb5-18b0a9330cc8";
-const postProfileImage =
-  "https://firebasestorage.googleapis.com/v0/b/instagram-clone-2a893.appspot.com/o/no-user-pic.png?alt=media&token=8b751397-f2d5-4354-b44b-75ad01c1bef9";
+function FeedPost(props) {
+  const { postImage, posterProfileImage, posterUsername } = props;
 
-function FeedPost() {
   return (
     <div className="Post">
       <div id="feed-post">
@@ -23,9 +18,9 @@ function FeedPost() {
         <div className="title-post">
           <img src={circle} className="circle" alt="circle" />
           <div className="profile-container">
-            <img src={postProfileImage} className="sonic" alt="sonic" />
+            <img src={posterProfileImage} className="sonic" alt="sonic" />
           </div>
-          <h3>User - Title</h3>
+          <h3>{posterUsername}</h3>
         </div>
         <div className="image-post">
           <img src={postImage} className="opm" alt="opm" />
--- src/components/GeneralFeed.js
+++ src/components/GeneralFeed.js
@@ -1,11 +1,50 @@
 import FeedPost from "./FeedPost";
 import ProfileArea from "./ProfileArea";
 
+const postImage =
+  "https://firebasestorage.googleapis.com/v0/b/instagram-clone-2a893.appspot.com/o/testingimage.jpg?alt=media&token=0d66dd18-63df-4ff8-bfb5-18b0a9330cc8";
+const posterProfileImage =
+  "https://firebasestorage.googleapis.com/v0/b/instagram-clone-2a893.appspot.com/o/no-user-pic.png?alt=media&token=8b751397-f2d5-4354-b44b-75ad01c1bef9";
+const posterUsername = "anonjohn";
+
+const object1 = {
+  postImage:
+    "https://firebasestorage.googleapis.com/v0/b/instagram-clone-2a893.appspot.com/o/testingimage.jpg?alt=media&token=0d66dd18-63df-4ff8-bfb5-18b0a9330cc8",
+  posterProfileImage:
+    "https://firebasestorage.googleapis.com/v0/b/instagram-clone-2a893.appspot.com/o/no-user-pic.png?alt=media&token=8b751397-f2d5-4354-b44b-75ad01c1bef9",
+  posterUsername: "anonjohn",
+};
+const object2 = {
+  postImage:
+    "https://firebasestorage.googleapis.com/v0/b/instagram-clone-2a893.appspot.com/o/Screen%20Shot%202022-06-30%20at%205.05.30%20PM.png?alt=media&token=1cb44f05-b98b-456f-9bb3-c881d0bbfcff",
+  posterProfileImage:
+    "https://firebasestorage.googleapis.com/v0/b/instagram-clone-2a893.appspot.com/o/no-user-pic.png?alt=media&token=8b751397-f2d5-4354-b44b-75ad01c1bef9",
+  posterUsername: "anonjohnz",
+};
+
+const array = [];
+
+array.push(object1);
+array.push(object2);
+
 const GeneralFeed = function (props) {
   return (
     <div className="general-body">
       <div className="feed-area">
-        <FeedPost />
+        {array.map((item) => {
+          return (
+            <FeedPost
+              postImage={item.postImage}
+              posterProfileImage={item.posterProfileImage}
+              posterUsername={item.posterUsername}
+            />
+          );
+        })}
+        {/* <FeedPost
+          postImage={postImage}
+          posterProfileImage={posterProfileImage}
+          posterUsername={posterUsername}
+        /> */}
       </div>
       <ProfileArea {...props} />
     </div>
